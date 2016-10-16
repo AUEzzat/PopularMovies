ded17b15a0db09a16a334b37f21c76e670b647d5
AUEzzat <amr.m.ezzat@gmail.com>
2016-10-17 01:51:23 +0200

    adding list with dummy data

--- app/src/main/java/com/example/android/app/popularmovies/MainActivityFragment.java
+++ app/src/main/java/com/example/android/app/popularmovies/MainActivityFragment.java
@@ -1,23 +1,34 @@
 package com.example.android.app.popularmovies;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
 
-    ArrayAdapter<String> popMoviesAdapter;
+    private ArrayAdapter<String> popMoviesAdapter;
 
     public MainActivityFragment() {
     }
@@ -25,17 +36,6 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        String[] data = {
-                "Mon 6/23 - Sunny - 31/17",
-                "Tue 6/24 - Foggy - 21/8",
-                "Wed 6/25 - Cloudy - 22/17",
-                "Thurs 6/26 - Rainy - 18/11",
-                "Fri 6/27 - Foggy - 21/10",
-                "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
-                "Sun 6/29 - Sunny - 20/7"
-        };
-        List<String> weekForecast = new ArrayList<String>(Arrays.asList(data));
-
 
         // Now that we have some dummy forecast data, create an ArrayAdapter.
         // The ArrayAdapter will take data from a source (like our dummy forecast) and
@@ -45,13 +45,135 @@ public class MainActivityFragment extends Fragment {
                         getActivity(), // The current context (this activity)
                         R.layout.popular_movies_layout, // The name of the layout ID.
                         R.id.popular_movies_textview, // The ID of the textview to populate.
-                        weekForecast);
-
+                        new ArrayList<String>());
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
+        // Get a reference to the ListView, and attach this adapter to it.
         ListView listView = (ListView) rootView.findViewById(R.id.popular_movies_list);
         listView.setAdapter(popMoviesAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                String forecast = popMoviesAdapter.getItem(position);
+//                Intent intent = new Intent(getActivity(), DetailActivity.class)
+//                        .putExtra(Intent.EXTRA_TEXT, forecast);
+//                startActivity(intent);
+            }
+        });
         return  rootView;
+    }
+
+    private void updateMovieList() {
+        FetchMoviesData movieTask = new FetchMoviesData();
+//        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+//        String location = prefs.getString(getString(R.string.pref_location_key),
+//                getString(R.string.pref_location_default));
+        movieTask.execute();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        updateMovieList();
+    }
+
+    public class FetchMoviesData extends AsyncTask<Void, Void, String[]> {
+
+        private final String LOG_TAG = FetchMoviesData.class.getSimpleName();
+
+        private String[] getMoviesDataFromJson(String moviesJsonStr) throws JSONException {
+
+            JSONObject forecastJson = new JSONObject(moviesJsonStr);
+            JSONArray moviesArray = forecastJson.getJSONArray("results");
+
+            String[] resultStrs = new String[moviesArray.length()];
+            String movieTitle="";
+            for(int i = 0; i < moviesArray.length(); i++) {
+                JSONObject eachMovie = moviesArray.getJSONObject(i);
+                movieTitle = eachMovie.getString("title");
+                resultStrs[i] = movieTitle;
+            }
+            return resultStrs;
+        }
+        @Override
+        protected String[] doInBackground(Void... params) {
+//            if (params.length == 0) {
+//                return null;
+//            }
+
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            // Will contain the raw JSON response as a string.
+            String moviesJsonStr = null;
+            String movieUrl = "http://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=80a833a4763920b2c735c6e5e3911338";
+            try {
+                URL url = new URL(movieUrl);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                // Read the input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    // Nothing to do.
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                    // But it does make debugging a *lot* easier if you print out the completed
+                    // buffer for debugging.
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    // Stream was empty.  No point in parsing.
+                    return null;
+                }
+                moviesJsonStr = buffer.toString();
+            }
+            catch (IOException e){
+                Log.e(LOG_TAG, "Error ", e);
+                // If the code didn't successfully get the weather data, there's no point in attemping
+                // to parse it.
+                return null;
+            }
+            // Create the request to OpenWeatherMap, and open the connection
+            finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e(LOG_TAG, "Error closing stream", e);
+                    }
+                }
+            }
+            try {
+                return getMoviesDataFromJson(moviesJsonStr);
+            } catch (JSONException e) {
+                Log.e(LOG_TAG, e.getMessage(), e);
+                e.printStackTrace();
+            }
+
+            // This will only happen if there was an error getting or parsing the forecast.
+            return null;
+        }
+        @Override
+        protected void onPostExecute(String[] result) {
+            if (result != null) {
+                popMoviesAdapter.clear();
+                for (String dayForecastStr : result) {
+                    popMoviesAdapter.add(dayForecastStr);
+                }
+            }
+        }
     }
 }
