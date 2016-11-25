3f2df63455c390e86354ffd2f5ddc0998213288d
AUEzzat <amr.m.ezzat@gmail.com>
2016-11-25 18:33:43 +0200

    intent using parcelable movie reviews in movieDetail activity

--- app/src/main/java/com/example/android/app/popularmovies/DetailActivity.java
+++ app/src/main/java/com/example/android/app/popularmovies/DetailActivity.java
@@ -1,17 +1,33 @@
 package com.example.android.app.popularmovies;
 
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Base64;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -25,7 +41,8 @@ public class DetailActivity extends AppCompatActivity {
 
         private static final String LOG_TAG = DetailActivityFragment.class.getSimpleName();
 
-        private String movieStr[];
+        private MovieDetail movie;
+        private ArrayAdapter<String> movieReviewsAdapter;
 
         public DetailActivityFragment() {
             setHasOptionsMenu(true);
@@ -34,31 +51,142 @@ public class DetailActivity extends AppCompatActivity {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
+
+            movieReviewsAdapter = new ArrayAdapter<String>(
+                            getActivity(), // The current context (this activity)
+                            R.layout.movie_reviews_layout, // The name of the layout ID.
+                            R.id.movie_review_layout_textview, // The ID of the textview to populate.
+                            new ArrayList<String>());
             Intent intent = getActivity().getIntent();
             View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
-            if (intent != null && intent.hasExtra(Intent.EXTRA_TEXT)) {
-                movieStr = intent.getStringExtra(Intent.EXTRA_TEXT).split("░");
-                ((TextView) rootView.findViewById(R.id.movie_title)).setText(movieStr[0]);
-                ((TextView) rootView.findViewById(R.id.release_date)).setText(movieStr[1]);
-                ((ImageView) rootView.findViewById(R.id.movie_poster)).setImageBitmap(StringToBitMap(movieStr[2]));
-                ((TextView) rootView.findViewById(R.id.vote_average)).setText(movieStr[3]);
-                ((TextView) rootView.findViewById(R.id.plot_synopsis)).setText(movieStr[4]);
+            ListView listView = (ListView) rootView.findViewById(R.id.movie_reviews_list);
+            listView.setAdapter(movieReviewsAdapter);
+            if (intent != null && intent.hasExtra("movie_detail")) {
+                movie = intent.getExtras().getParcelable("movie_detail");
+                ((TextView) rootView.findViewById(R.id.movie_title)).setText(movie.getMovieTitle());
+                ((TextView) rootView.findViewById(R.id.release_date)).setText(movie.getReleaseDateStr());
+                ((ImageView) rootView.findViewById(R.id.movie_poster)).setImageBitmap(movie.getMoviePoster());
+                ((TextView) rootView.findViewById(R.id.vote_average)).setText(movie.getVoteAverageStr());
+                ((TextView) rootView.findViewById(R.id.plot_synopsis)).setText(movie.getPlotSynopsis());
             }
             return rootView;
         }
 
-        /**
-         * @param encodedString
-         * @return bitmap (from given string)
-         */
-        private static Bitmap StringToBitMap(String encodedString){
-            try{
-                byte [] encodeByte= Base64.decode(encodedString,Base64.DEFAULT);
-                Bitmap bitmap= BitmapFactory.decodeByteArray(encodeByte, 0, encodeByte.length);
-                return bitmap;
-            }catch(Exception e){
-                e.getMessage();
+        private void updateMovieDetails() {
+            FetchMovieReviews movieReviewsTask = new FetchMovieReviews();
+            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+            String api_key = prefs.getString(getString(R.string.pref_api_key), getString(R.string.pref_api_value));
+            String movieID = movie.getMovieID();
+            movieReviewsTask.execute(api_key, movieID);
+        }
+
+        @Override
+        public void onStart() {
+            super.onStart();
+            updateMovieDetails();
+        }
+
+        public class FetchMovieReviews extends AsyncTask<String, Void, ArrayList<String>> {
+
+            private final String LOG_TAG = FetchMovieReviews.class.getSimpleName();
+
+            private ArrayList<String> getMovieReviewsFromJson(String movieReviewsJsonStr) throws JSONException, IOException {
+
+                JSONObject forecastJson = new JSONObject(movieReviewsJsonStr);
+                JSONArray moviesReviewsArray = forecastJson.getJSONArray("results");
+                ArrayList<String> movieReviews = new ArrayList<>(moviesReviewsArray.length());
+
+                for (int i = 0; i < moviesReviewsArray.length(); i++) {
+                    JSONObject eachMovie = moviesReviewsArray.getJSONObject(i);
+                    String reviewAuthor = eachMovie.getString("author");
+                    String reviewContent = eachMovie.getString("content");
+                    String review = reviewContent;
+                    movieReviews.add(reviewAuthor + ":\n\n" + review);
+                }
+                return movieReviews;
+            }
+
+            @Override
+            protected ArrayList<String> doInBackground(String... params) {
+
+                HttpURLConnection urlConnection = null;
+                BufferedReader reader = null;
+
+                // Will contain the raw JSON response as a string.
+                String movieReviewsJsonStr = null;
+                String myApiKey = params[0];
+                String movieID = params[1];
+                try {
+                    Uri.Builder movieUrl = new Uri.Builder();
+                    movieUrl.scheme("https")
+                            .authority("api.themoviedb.org")
+                            .appendPath("3")
+                            .appendPath("movie")
+                            .appendPath(movieID)
+                            .appendPath("reviews")
+                            .appendQueryParameter("api_key", myApiKey);
+                    URL url = new URL(movieUrl.toString());
+                    urlConnection = (HttpURLConnection) url.openConnection();
+                    urlConnection.setRequestMethod("GET");
+                    urlConnection.connect();
+
+                    // Read the input stream into a String
+                    InputStream inputStream = urlConnection.getInputStream();
+                    StringBuffer buffer = new StringBuffer();
+                    if (inputStream == null) {
+                        // Nothing to do.
+                        return null;
+                    }
+                    reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        buffer.append(line + "\n");
+                    }
+
+                    if (buffer.length() == 0) {
+                        // Stream was empty.  No point in parsing.
+                        return null;
+                    }
+                    movieReviewsJsonStr = buffer.toString();
+                } catch (IOException e) {
+                    Log.e(LOG_TAG, "Error ", e);
+                    return null;
+                } finally {
+                    if (urlConnection != null) {
+                        urlConnection.disconnect();
+                    }
+                    if (reader != null) {
+                        try {
+                            reader.close();
+                        } catch (final IOException e) {
+                            Log.e(LOG_TAG, "Error closing stream", e);
+                        }
+                    }
+                }
+                try {
+                    return getMovieReviewsFromJson(movieReviewsJsonStr);
+                } catch (JSONException e) {
+                    Log.e(LOG_TAG, e.getMessage(), e);
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    Log.e(LOG_TAG, e.getMessage(), e);
+                    e.printStackTrace();
+                }
                 return null;
+            }
+
+            @Override
+            protected void onPostExecute(ArrayList<String> movieReviews) {
+                if (movieReviews != null) {
+                    movieReviewsAdapter.clear();
+                    for (String movieReview : movieReviews) {
+                        movieReviewsAdapter.add(movieReview);
+                    }
+                }
+                if(movieReviews.size() == 0){
+                    movieReviewsAdapter.add("No reviews yet.");
+                }
             }
         }
     }
--- app/src/main/java/com/example/android/app/popularmovies/MovieDetail.java
+++ app/src/main/java/com/example/android/app/popularmovies/MovieDetail.java
@@ -1,34 +1,28 @@
 package com.example.android.app.popularmovies;
 
 import android.graphics.Bitmap;
-import android.util.Base64;
-
-import java.io.ByteArrayOutputStream;
+import android.os.Parcel;
+import android.os.Parcelable;
 
 /**
  * Created by amrezzat on 10/21/2016.
  */
 
-public class MovieDetail {
+public class MovieDetail implements Parcelable {
 
+    private String movieID;
     private String movieTitle;
     private int releaseDate;
+    private String releaseDateStr;
     private Bitmap moviePoster;
     private double voteAverage;
-    private Integer voteCount;
+    private String voteAverageStr;
+    private int voteCount;
     private String plotSynopsis;
 
-//    public MovieDetail(){
-//        this.movieTitle = "";
-//        this.releaseDate = 0;
-//        Bitmap.Config conf = Bitmap.Config.ARGB_8888; // see other conf types
-//        this.moviePoster = Bitmap.createBitmap(200, 200, conf);
-//        this.voteAverage = 0.0;
-//        this.plotSynopsis = "";
-//    }
-
-    public MovieDetail(String movieTitle, int releaseDate, Bitmap moviePoster,
-                       double voteAverage, Integer voteCount, String plotSynopsis){
+    public MovieDetail(String movieID, String movieTitle, int releaseDate, Bitmap moviePoster,
+                       double voteAverage, int voteCount, String plotSynopsis) {
+        this.movieID = movieID;
         this.movieTitle = movieTitle;
         this.releaseDate = releaseDate;
         this.moviePoster = moviePoster;
@@ -37,34 +31,76 @@ public class MovieDetail {
         this.plotSynopsis = plotSynopsis;
     }
 
-    public Bitmap getMoviePoster(){
+    public String getMovieID() {
+        return movieID;
+    }
+
+    public String getMovieTitle() {
+
+        return movieTitle;
+    }
+
+    public String getReleaseDateStr() {
+        releaseDateStr = "????";
+        if (releaseDate != 0)
+            releaseDateStr = Integer.toString(releaseDate);
+        return releaseDateStr;
+    }
+
+    public String getVoteAverageStr() {
+        if (voteCount == 0)
+            voteAverageStr = "Not Rated";
+        else
+            voteAverageStr = voteAverage + "/10";
+        return voteAverageStr;
+    }
+
+    public String getPlotSynopsis() {
+        if (plotSynopsis.length() == 0)
+            plotSynopsis = "No overview found.";
+        return plotSynopsis;
+    }
+
+    public Bitmap getMoviePoster() {
         return moviePoster;
     }
 
-    public String getString(){
-        String voteAveragePreview;
-        String releaseDatePreview = "????";
-
-        if(plotSynopsis.length() == 0)
-            plotSynopsis = "No overview found.";
-        if(releaseDate != 0)
-            releaseDatePreview = Integer.toString(releaseDate);
-        if(voteCount == 0)
-            voteAveragePreview = "Not Rated";
-        else
-            voteAveragePreview = voteAverage+"/10";
-        return (movieTitle+"░"+releaseDatePreview+"░"+BitMapToString(moviePoster)+"░"+voteAveragePreview+"░"+plotSynopsis);
+    protected MovieDetail(Parcel in) {
+        movieID = in.readString();
+        movieTitle = in.readString();
+        releaseDate = in.readInt();
+        moviePoster = (Bitmap) in.readValue(Bitmap.class.getClassLoader());
+        voteAverage = in.readDouble();
+        voteCount = in.readInt();
+        plotSynopsis = in.readString();
     }
 
-    /**
-     * @param bitmap
-     * @return converting bitmap and return a string
-     */
-    private static String BitMapToString(Bitmap bitmap){
-        ByteArrayOutputStream baos=new  ByteArrayOutputStream();
-        bitmap.compress(Bitmap.CompressFormat.PNG,100, baos);
-        byte [] b=baos.toByteArray();
-        String temp= Base64.encodeToString(b, Base64.DEFAULT);
-        return temp;
+    @Override
+    public int describeContents() {
+        return 0;
     }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(movieID);
+        dest.writeString(movieTitle);
+        dest.writeInt(releaseDate);
+        dest.writeValue(moviePoster);
+        dest.writeDouble(voteAverage);
+        dest.writeInt(voteCount);
+        dest.writeString(plotSynopsis);
+    }
+
+    @SuppressWarnings("unused")
+    public static final Parcelable.Creator<MovieDetail> CREATOR = new Parcelable.Creator<MovieDetail>() {
+        @Override
+        public MovieDetail createFromParcel(Parcel in) {
+            return new MovieDetail(in);
+        }
+
+        @Override
+        public MovieDetail[] newArray(int size) {
+            return new MovieDetail[size];
+        }
+    };
 }
