71a52a5ff0d9722309b64e64843d3f4da9fb2ac5
AUEzzat <amr.m.ezzat@gmail.com>
2016-12-01 00:51:28 +0200

    up button doesn't reinitialize parent activity

--- app/src/main/java/com/example/android/app/popularmovies/DetailActivity.java
+++ app/src/main/java/com/example/android/app/popularmovies/DetailActivity.java
@@ -1,10 +1,12 @@
 package com.example.android.app.popularmovies;
 
+import android.annotation.TargetApi;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
@@ -43,6 +45,7 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
     }
 
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
     @Override
     public Intent getParentActivityIntent() {
         return super.getParentActivityIntent().addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
--- app/src/main/java/com/example/android/app/popularmovies/SettingsActivity.java
+++ app/src/main/java/com/example/android/app/popularmovies/SettingsActivity.java
@@ -1,6 +1,8 @@
 package com.example.android.app.popularmovies;
 
+import android.annotation.TargetApi;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.ListPreference;
 import android.preference.Preference;
@@ -18,6 +20,7 @@ public class SettingsActivity extends AppCompatActivity {
         getFragmentManager().beginTransaction().replace(android.R.id.content, new SettingsFragment()).commit();
     }
 
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
     @Override
     public Intent getParentActivityIntent() {
         return super.getParentActivityIntent().addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
